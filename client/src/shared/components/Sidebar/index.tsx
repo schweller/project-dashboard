dc94c436d137813e4dbf3acfe7f2cb03626dcb82
Inácio Schweller <iscmendonca@gmail.com>
2020-03-23 22:47:21 -0300

    styling the sidebar

--- client/src/shared/components/Sidebar/index.tsx
+++ client/src/shared/components/Sidebar/index.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTachometerAlt,
+  faStickyNote
+} from "@fortawesome/free-solid-svg-icons";
 
-const Sidebar = () =>
-  <StyledNav>    
+const dashboardIcon = <FontAwesomeIcon icon={faTachometerAlt} size="2x" />;
+const storiesIcon = <FontAwesomeIcon icon={faStickyNote} size="2x" />;
+
+const Sidebar = () => (
+  <StyledNav>
     <InnerNavigation>
-      <li>Dashboard</li>
-      <li>Epics</li>
-      <li>Stories</li>
-      <li>Projects</li>
+      <li>
+        {dashboardIcon}
+        <InnerNavigationLabel>Dashboard</InnerNavigationLabel>
+      </li>
+      <li>
+        {storiesIcon}
+        <InnerNavigationLabel>Stories</InnerNavigationLabel>
+      </li>
     </InnerNavigation>
   </StyledNav>
+);
 
-const StyledNav =  styled.nav`
-  background: pink;
+const StyledNav = styled.nav`
+  background: #f7f7f7;
+  border-right: 1px solid #d0d0d0;
   box-sizing: border-box;
   height: 100%;
   left: 0;
@@ -20,12 +34,13 @@ const StyledNav =  styled.nav`
   padding: 74px 0 0;
   position: fixed;
   top: 0;
-  transition: background-color 120ms ease-in-out,padding-top .6s ease-in-out,width 250ms ease-in-out;
+  transition: background-color 120ms ease-in-out, padding-top 0.6s ease-in-out,
+    width 250ms ease-in-out;
   width: 80px;
   // &:hover {
   //   width: 200px;
   // }
-`
+`;
 
 const InnerNavigation = styled.ul`
   list-style: none;
@@ -33,14 +48,23 @@ const InnerNavigation = styled.ul`
   padding: 0;
   > li {
     display: flex;
-    padding: 10px 0;
-    height: 60px;
+    flex-direction: column;
     align-items: center;
-    border-bottom: 1px solid black;
-    &:hover {
-      background: gray;
+    padding: 20px 0 10px;
+    height: 60px;
+    border-top: 1px solid #d0d0d0;
+    color: purple;
+    &:first-child {
+      border: none;
     }
   }
-`
+`;
+
+const InnerNavigationLabel = styled.span`
+  font-family: "Inter", sans-serif;
+  font-size: 10px;
+  text-transform: uppercase;
+  margin-top: 10px;
+`;
 
 export default Sidebar;
